c3aa56cef78ce085c7aa336e2f28a175c283c3ec
tomwilson <tomas.f.wilson@gmail.com>
2021-10-20 13:09:06 -0300

    test: ensure AxiosHttpClient returns correct values

--- tests/infra/http/axios-http-client.spec.ts
+++ tests/infra/http/axios-http-client.spec.ts
@@ -25,11 +25,28 @@ const mockPostRequest = (): HttpPostParams<unknown> => ({
   url: faker.internet.url(),
   body: faker.random.objectElement(),
 });
+
+const mockedAxiosResult = {
+  data: faker.random.objectElement(),
+  status: faker.datatype.number(),
+};
+
 describe('AxiosHttpClient', () => {
   test('Should call axios with correct values', async () => {
     const { mockedAxios, sut } = makesut();
+    mockedAxios.post.mockResolvedValue(mockedAxiosResult);
     const request = mockPostRequest();
     await sut.post(request);
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
+  });
+
+  test('Should return the correct status code and body', async () => {
+    const { mockedAxios, sut } = makesut();
+    mockedAxios.post.mockResolvedValue(mockedAxiosResult);
+    const httpResponse = await sut.post(mockPostRequest());
+    expect(httpResponse).toEqual({
+      statusCode: mockedAxiosResult.status,
+      body: mockedAxiosResult.data,
+    });
   });
 });
