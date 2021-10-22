39de3e47844bb6765f85da99e9425cc0a811a68d
tomwilson <tomas.f.wilson@gmail.com>
2021-10-21 23:21:19 -0300

    refactor: move SUT creator to factory function

--- tests/validation/validators/required-field.spec.ts
+++ tests/validation/validators/required-field.spec.ts
@@ -2,16 +2,20 @@ import { RequiredFieldError } from '@/validation/errors';
 import { RequiredFieldValidation } from '@/validation/validators';
 import faker from 'faker';
 
+const makeSut = (): RequiredFieldValidation => {
+  return new RequiredFieldValidation(faker.database.column());
+};
+
 describe('RequiredFieldValidation', () => {
   test('Should return error if field is empty', () => {
-    const sut = new RequiredFieldValidation('email');
+    const sut = makeSut();
     const error = sut.validate('');
     expect(error).toEqual(new RequiredFieldError());
   });
 
   test('Should return null if field is not empty', () => {
-    const sut = new RequiredFieldValidation('email');
-    const error = sut.validate(faker.internet.email());
+    const sut = makeSut();
+    const error = sut.validate(faker.random.word());
     expect(error).toBeFalsy();
   });
 });
