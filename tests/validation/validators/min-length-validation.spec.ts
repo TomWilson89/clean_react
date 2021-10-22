c2829ca3f46e2871e9b5d09c92770feedef12aee
tomwilson <tomas.f.wilson@gmail.com>
2021-10-22 07:51:31 -0300

    refactor move SUT to its own factory function

--- tests/validation/validators/min-length-validation.spec.ts
+++ tests/validation/validators/min-length-validation.spec.ts
@@ -1,16 +1,22 @@
 import { MinLengthError } from '@/validation/errors';
 import { MinLengthValidation } from '@/validation/validators';
+import faker from 'faker';
 
+const minLength = faker.datatype.number();
+
+const makeSut = (): MinLengthValidation => {
+  return new MinLengthValidation(faker.database.column(), minLength);
+};
 describe('MinLengthValidation', () => {
   test('Should return error if value  is invalid', () => {
-    const sut = new MinLengthValidation('field', 5);
-    const error = sut.validate('123');
+    const sut = makeSut();
+    const error = sut.validate(faker.random.alphaNumeric(minLength - 1));
     expect(error).toEqual(new MinLengthError());
   });
 
   test('Should return falsy if value  is valid', () => {
-    const sut = new MinLengthValidation('field', 5);
-    const error = sut.validate('123555');
+    const sut = makeSut();
+    const error = sut.validate(faker.random.alphaNumeric(minLength));
     expect(error).toBeFalsy();
   });
 });
