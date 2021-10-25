e035b912b15a70aa5315ea99be805e697992bf1d
tomwilson <tomas.f.wilson@gmail.com>
2021-10-25 15:47:31 -0300

    test: ensure login presents valid state if form is valid

--- tests/e2e/cypress/integration/login.spec.ts
+++ tests/e2e/cypress/integration/login.spec.ts
@@ -34,4 +34,15 @@ describe('Login', () => {
     cy.getByTestId('submit').should('have.attr', 'disabled');
     cy.getByTestId('error-wrap').should('not.have.descendants');
   });
+
+  it('Should present valid state if form is valid', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email());
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5));
+
+    cy.getByTestId('email-status').should('contain.text', '🟢');
+    cy.getByTestId('password-status').should('contain.text', '🟢');
+
+    cy.getByTestId('submit').should('not.have.attr', 'disabled');
+    cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
 });
