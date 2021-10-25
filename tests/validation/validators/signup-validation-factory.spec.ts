dc1e3cc8538f81fde8e9321065e3ad02acb48709
tomwilson <tomas.f.wilson@gmail.com>
2021-10-25 12:59:53 -0300

    test: add SignupValidationFactory

---
+++ tests/validation/validators/signup-validation-factory.spec.ts
@@ -0,0 +1,21 @@
+import { makeSignupValidation } from '@/main/factories/validation';
+import { ValidationBuilder } from '@/validation/builder';
+import { ValidationComposite } from '@/validation/composite';
+
+describe('SignupValidationFactory', () => {
+  test('should make ValidationComposite with correct validations', () => {
+    const composite = makeSignupValidation();
+
+    expect(composite).toEqual(
+      ValidationComposite.build([
+        ...ValidationBuilder.field('name').required().min(3).build(),
+        ...ValidationBuilder.field('email').required().email().build(),
+        ...ValidationBuilder.field('password').required().min(5).build(),
+        ...ValidationBuilder.field('passwordConfirmation')
+          .required()
+          .min(5)
+          .build(),
+      ])
+    );
+  });
+});
