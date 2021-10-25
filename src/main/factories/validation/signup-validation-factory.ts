1638a8e015af55a69c6d69058ff48868873a1d00
tomwilson <tomas.f.wilson@gmail.com>
2021-10-25 12:59:16 -0300

    feat: add SignupValidationFactory

---
+++ src/main/factories/validation/signup-validation-factory.ts
@@ -0,0 +1,14 @@
+import { ValidationBuilder } from '@/validation/builder';
+import { ValidationComposite } from '@/validation/composite';
+
+export const makeSignupValidation = (): ValidationComposite => {
+  return ValidationComposite.build([
+    ...ValidationBuilder.field('name').required().min(3).build(),
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().min(5).build(),
+    ...ValidationBuilder.field('passwordConfirmation')
+      .required()
+      .min(5)
+      .build(),
+  ]);
+};
