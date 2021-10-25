4adc4b1919f4a1d420e09d2d7871297bf983ad0d
tomwilson <tomas.f.wilson@gmail.com>
2021-10-25 08:57:20 -0300

    test: ensure Signup change name status to error if validation fails

--- tests/presentation/signup.spec.tsx
+++ tests/presentation/signup.spec.tsx
@@ -1,22 +1,55 @@
 import { Signup } from '@/presentation/pages';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import faker from 'faker';
 import React from 'react';
 import { Helper } from './helper';
+import { ValidationSpy } from './mocks';
 
-const makeSut = (): void => {
-  render(<Signup />);
+type SutParams = {
+  validationError: string;
+};
+
+type SutTypes = {
+  validationSpy: ValidationSpy;
+};
+
+const makeSut = (params?: SutParams): SutTypes => {
+  const validationSpy = new ValidationSpy();
+  validationSpy.errorMessage = params?.validationError;
+  render(<Signup validation={validationSpy} />);
+
+  return {
+    validationSpy,
+  };
+};
+
+const populateField = (
+  fieldName: string,
+  value = faker.random.word()
+): void => {
+  const input = screen.getByTestId(fieldName);
+  fireEvent.input(input, {
+    target: { value },
+  });
 };
 
 describe('Signup component', () => {
-  makeSut();
-  const validationError = 'Required';
   test('should start with initial state', () => {
+    const validationError = faker.random.word();
+    makeSut({ validationError });
     Helper.testChildCount('error-wrap', 0);
     Helper.testButtonIsDisable('submit', true);
     Helper.testFieldStatus('name', validationError);
-    Helper.testFieldStatus('email', validationError);
-    Helper.testFieldStatus('password', validationError);
-    Helper.testFieldStatus('passwordConfirmation', validationError);
+    Helper.testFieldStatus('email', 'Required');
+    Helper.testFieldStatus('password', 'Required');
+    Helper.testFieldStatus('passwordConfirmation', 'Required');
+  });
+
+  test('Should show name error if validation fails', () => {
+    const validationError = faker.random.word();
+    makeSut({ validationError });
+    populateField('name');
+    Helper.testFieldStatus('name', validationError);
   });
 });
