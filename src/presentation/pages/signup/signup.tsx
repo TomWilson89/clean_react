8ed5c1727bec0006d90e83675eccc35c2b7e7f52
tomwilson <tomas.f.wilson@gmail.com>
2021-10-25 10:05:39 -0300

    feat: ensure Signup prevent AddAccuont to be called multiple times

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -64,6 +64,9 @@ const Signup: React.FC<Props> = ({ validation, addAccount }: Props) => {
     event: React.FormEvent<HTMLFormElement>
   ): Promise<void> => {
     event.preventDefault();
+    if (state.isLoading) {
+      return;
+    }
     setState({ ...state, isLoading: true });
     await addAccount.add({
       email: state.email,
