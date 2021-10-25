3ecd1797ee5abfd2df407c8f6aa888c89fabea23
tomwilson <tomas.f.wilson@gmail.com>
2021-10-25 15:44:32 -0300

    test: ensure login present error state if form is invalid

--- tests/e2e/cypress/integration/login.spec.ts
+++ tests/e2e/cypress/integration/login.spec.ts
@@ -1,15 +1,36 @@
+import faker from 'faker';
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login');
   });
 
   it('Should load with correct initial state', () => {
+    cy.getByTestId('email').should('have.attr', 'readOnly');
+    cy.getByTestId('password').should('have.attr', 'readOnly');
     cy.getByTestId('email-status')
       .should('have.attr', 'title', 'Required')
       .should('contain.text', '🔴');
+
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Required')
       .should('contain.text', '🔴');
+
+    cy.getByTestId('submit').should('have.attr', 'disabled');
+    cy.getByTestId('error-wrap').should('not.have.descendants');
+  });
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('email').focus().type(faker.random.word());
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3));
+
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Invalid Field')
+      .should('contain.text', '🔴');
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Field Is Too Short')
+      .should('contain.text', '🔴');
+
     cy.getByTestId('submit').should('have.attr', 'disabled');
     cy.getByTestId('error-wrap').should('not.have.descendants');
   });
