46441ebbfa5f7af920fe62a6dd67e1ab91e44bf7
tomwilson <tomas.f.wilson@gmail.com>
2021-10-25 21:42:27 -0300

    refactor: add input animations

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,5 +1,8 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { Context } from '..';
 import Styles from './input-styles.scss';
 
@@ -9,8 +12,9 @@ type Props = React.DetailedHTMLProps<
 >;
 
 const Input: React.FC<Props> = (props: Props) => {
-  const { name } = props;
+  const { name, placeholder } = props;
   const { state, setState } = useContext(Context);
+  const inputRef = useRef<HTMLInputElement>();
 
   const error = state[`${name}Error`];
 
@@ -19,40 +23,30 @@ const Input: React.FC<Props> = (props: Props) => {
     event.target.readOnly = false;
   };
 
-  const getStatus = (errorText: string): string | null => {
-    let text = '🟢';
-    if (errorText) text = '🔴';
-    return text;
-  };
-
-  const getTitle = (errorText: string): string => {
-    let text = '';
-    if (errorText) text = errorText;
-    return text;
-  };
-
-  const handleChange = (event: React.FocusEvent<HTMLInputElement>): void => {
+  const handleChange = (event: React.FocusEvent<HTMLInputElement>): void =>
     setState({
       ...state,
       [event.target.name]: event.target.value,
     });
-  };
 
   return (
     <div className={Styles.inputWrap}>
       <input
+        ref={inputRef}
         {...props}
+        placeholder=" "
         data-testid={name}
         readOnly
         onFocus={enableInput}
         onChange={handleChange}
       />
+      <label onClick={() => inputRef.current.focus()}>{placeholder}</label>
       <span
         data-testid={`${name}-status`}
-        title={getTitle(error)}
+        title={error || ''}
         className={Styles.status}
       >
-        {getStatus(error)}
+        {error ? '🔴' : '🟢'}
       </span>
     </div>
   );
