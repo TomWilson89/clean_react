0849822e183eec43053244f7261f042583ee1437
tomwilson <tomas.f.wilson@gmail.com>
2021-11-01 13:28:55 -0300

    feat: ensure RemoteLoadSurveyResult returns survey result model on 200

--- src/data/usecases/remote-load-survey-result.ts
+++ src/data/usecases/remote-load-survey-result.ts
@@ -1,18 +1,22 @@
 import { AccessDeniedError, UnexpectedError } from '@/domain/errors';
+import { LoadSurveyResult } from '@/domain/usecases';
 import { HttpGetClient, HttpStatusCode } from '../protocols/http';
 
-export class RemoteLoadSurveyResult {
+export class RemoteLoadSurveyResult implements LoadSurveyResult {
   constructor(
     private readonly url: string,
-    private readonly httpGetClient: HttpGetClient
+    private readonly httpGetClient: HttpGetClient<RemoteLoadSurveyResult.Model>
   ) {}
 
-  async load(): Promise<void> {
+  async load(): Promise<LoadSurveyResult.Model> {
     const httpResponse = await this.httpGetClient.get({ url: this.url });
 
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok: {
-        return Promise.resolve();
+        const remoteSurveyResult = Object.assign(httpResponse.body, {
+          date: new Date(httpResponse.body.date),
+        });
+        return remoteSurveyResult;
       }
 
       case HttpStatusCode.forbidden: {
@@ -23,4 +27,17 @@ export class RemoteLoadSurveyResult {
         throw new UnexpectedError();
     }
   }
+}
+
+export namespace RemoteLoadSurveyResult {
+  export type Model = {
+    question: string;
+    date: string;
+    answers: {
+      image?: string;
+      answer: string;
+      count: number;
+      percent: number;
+    }[];
+  };
 }
--- src/domain/usecases/load-survey-result.ts
+++ src/domain/usecases/load-survey-result.ts
@@ -6,13 +6,11 @@ export namespace LoadSurveyResult {
   export type Model = {
     question: string;
     date: Date;
-    answers: [
-      {
-        image?: string;
-        answer: string;
-        count: number;
-        percent: number;
-      }
-    ];
+    answers: {
+      image?: string;
+      answer: string;
+      count: number;
+      percent: number;
+    }[];
   };
 }
