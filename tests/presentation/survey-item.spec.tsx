9e7bd1cd04a17b17bb566ce851203799ea964e9b
tomwilson <tomas.f.wilson@gmail.com>
2021-11-01 16:21:34 -0300

    fix: move Calendar tests back to survey list

--- tests/presentation/survey-item.spec.tsx
+++ tests/presentation/survey-item.spec.tsx
@@ -11,16 +11,21 @@ describe('SurveyItem', () => {
   test('should render with correct values ', () => {
     const survey = Object.assign(mockSurveyModel(), {
       didAnswer: true,
+      date: new Date('2020-01-10T00:00:00'),
     });
     makeSut(survey);
 
     expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp);
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question);
+    expect(screen.getByTestId('day')).toHaveTextContent('10');
+    expect(screen.getByTestId('month')).toHaveTextContent('ene');
+    expect(screen.getByTestId('year')).toHaveTextContent('2020');
   });
 
   test('should render with correct values ', () => {
     const survey = Object.assign(mockSurveyModel(), {
       didAnswer: false,
+      date: new Date('2019-02-03T00:00:00'),
     });
     makeSut(survey);
 
@@ -30,5 +35,8 @@ describe('SurveyItem', () => {
     );
 
     expect(screen.getByTestId('question')).toHaveTextContent(survey.question);
+    expect(screen.getByTestId('day')).toHaveTextContent('03');
+    expect(screen.getByTestId('month')).toHaveTextContent('feb');
+    expect(screen.getByTestId('year')).toHaveTextContent('2019');
   });
 });
